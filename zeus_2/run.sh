6bb8b4546849c0b18d964c47c4e229b760192c7b
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-05-31 21:25:05 +0200

    Decrease walltime to 20 minutes

--- zeus_2/run.sh
+++ zeus_2/run.sh
@@ -8,5 +8,5 @@ Grant=grant_name
 
 for i in $(seq $MinNodes $Step $MaxNodes); do
   printf $i
-  qsub -q l_short -N emas_$i -A $Grant -l nodes=$i:ppn=12 -l walltime=03:00:00 $HOME/erl-scripts/run_nodes.sh
+  qsub -q l_short -N emas_$i -A $Grant -l nodes=$i:ppn=12 -l walltime=00:20:00 $HOME/erl-scripts/run_nodes.sh
 done
