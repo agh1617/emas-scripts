556f21dfe01efcc8e9e071f3e75fbd941247671a
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-10-25 00:06:10 +0200

    Use hostname as erl sname

--- prometheus/run_node.sh
+++ prometheus/run_node.sh
@@ -8,15 +8,16 @@ module load erlang/17.5
 job_dir=$1
 job_id=$2
 
+hostname=`hostname`
 emas_root=$HOME/ppagh/erlang-emas
-logs_path="$job_dir/`hostname`"
+logs_path=$job_dir/$hostname
 mkdir -p $logs_path
 
 duration=90000 # 90 seconds
 world_migration_probability=0.001
 
 cd $job_dir
-erl -sname emas -setcookie $job_id -pa $emas_root/ebin \
+erl -sname $hostname -setcookie $job_id -pa $emas_root/ebin \
   -pa $emas_root/deps/*/ebin -noshell -detach \
   -eval "emas:start($simulation_duration, [{model, mas_hybrid}, {islands, 24}, {log_dir, \"$logs_path\"}, {world_migration_probability, \"$world_migration_probability\"}])." \
   -run init stop
