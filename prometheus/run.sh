7c54f8cc5a01c40ce65768ddaa37e6feb7726fd3
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-11-01 18:24:50 +0100

    More descriptive sbatch flags

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -7,17 +7,19 @@ max_nodes=100
 step=2
 
 grant=plglogin
+partition=plgrid
+time="00:20:00"
 
 mkdir -p "$SCRATCH/ppagh"
 
 for num_nodes in $(seq $min_nodes $step $max_nodes); do
   printf $num_nodes
 
-  sbatch -p plgrid \
-         -A $grant \
-         -N $num_nodes \
+  sbatch --partition=$partition \
+         --account=$grant \
+         --nodes=$num_nodes \
          --ntasks-per-node=24 \
-         --time=00:20:00 \
-         -J ppagh-emas-$num_nodes \
+         --time=$time \
+         --job-name=emas-$num_nodes \
          $HOME/ppagh/run-scripts/prometheus/run_nodes.sh $session_timestamp
 done
