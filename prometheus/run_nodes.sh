044102cd0eb44a1d17b5e05b8b7901fa77965893
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2016-11-01 20:44:03 +0100

    Generate .hosts.erlang inside job dir instead of home dir

--- prometheus/run_nodes.sh
+++ prometheus/run_nodes.sh
@@ -2,8 +2,6 @@
 
 session_timestamp=$1
 
-srun -l /bin/hostname | sort -n | awk '{printf "'\''%s'\''.\n", $2}' > $HOME/.hosts.erlang
-
 for i in $(seq 0 9); do
   job_id=`head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
 
@@ -12,5 +10,6 @@ for i in $(seq 0 9); do
   mkdir -p $job_dir
   cd $job_dir
 
+  srun -l /bin/hostname | sort -n | awk '{printf "'\''%s'\''.\n", $2}' > $job_dir/.hosts.erlang
   srun -o /dev/null $HOME/ppagh/run-scripts/prometheus/run_node.sh $job_dir $job_id
 done
